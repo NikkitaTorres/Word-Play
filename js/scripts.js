61456373bd3c305dd1525e1cf2b4f7039e9465fb
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-23 16:49:11 -0700

    Tie the final result to the window.addEventListener

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,14 @@ function handleForm(event){
   const userInput = document.querySelector("input[name=sentence]").value;
   const userSelectionsArray = userInput.split(' ');
   const filteredWords = userSelectionsArray.filter(word => word.length >= 3);
-  const Final = filteredWords.reverse();
+  const final = filteredWords.reverse();
 
   const resultsHeading = document.createElement("h2");
   resultsHeading.append("Goofed Version:");
   document.body.append(resultsHeading);
 
   const resultParagraph = document.createElement("p");
-  resultParagraph.append(resultString);
+  resultParagraph.append(final);
   document.body.append(resultParagraph);
 }
 
